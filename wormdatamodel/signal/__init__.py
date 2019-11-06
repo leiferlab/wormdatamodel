9c129c51e44abc3555e4f4b2ed2da2f7791d9672
Francesco Randi <francesco.randi@gmail.com>
2019-11-06 14:32:58 -0500

    Version number from git commit hash. signal to_file and from_file to save and load parameters and version.

--- wormdatamodel/signal/__init__.py
+++ wormdatamodel/signal/__init__.py
@@ -1,3 +1,4 @@
 __all__ = ['extraction']
 
 from .extraction import extract, _generate_box_indices, _slice_array
+from .file import to_file, from_file
---
+++ wormdatamodel/signal/file.py
@@ -0,0 +1,34 @@
+import numpy as np
+import wormdatamodel as wormdm
+import pkg_resources
+import json
+
+def to_file(Signal, folder, filename, method, other={}):
+    if folder[-1]!="/": folder+="/"
+    
+    v = pkg_resources.get_distribution("wormbrain").version
+    
+    header = {"method": method, "version": v}
+    for key in other:
+        try:
+            header[key] = other[key]
+        except:
+            pass
+    header = json.dumps(header)
+    
+    np.savetxt(folder+filename, Signal, header=header)
+    
+def from_file(folder, filename):
+    if folder[-1]!="/": folder+="/"
+    
+    f = open(folder+filename,"r")
+    l = f.readline()
+    f.close()
+    try:
+        info = json.loads(l[2:])
+    except:
+        info = {}
+        
+    Signal = np.loadtxt(folder+filename)
+    
+    return Signal, info
